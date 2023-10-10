0a1b5f546be833aade8996cddea7ccf9b9b41f4f
alex <o.lototskyi@gmail.com>
2023-10-10 18:32:30 +0300

    added header files with peripheral register addresses

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ D:/my/microcontrollers/STM32/projects/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+D:/my/microcontrollers/STM32/projects/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
---
+++ Debug/drivers/Src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,6 @@
+drivers/Src/stm32f407xx_gpio_driver.o: \
+ ../drivers/Src/stm32f407xx_gpio_driver.c \
+ D:/my/microcontrollers/STM32/projects/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ D:/my/microcontrollers/STM32/projects/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+D:/my/microcontrollers/STM32/projects/stm32f4xx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+D:/my/microcontrollers/STM32/projects/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
