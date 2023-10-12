45ac58bf219b03b1d3a5943f3b233dd2008cc201
alex <o.lototskyi@gmail.com>
2023-10-12 08:45:10 +0300

    GPIO driver functionality, examples how to work with GPIO

--- drivers/Inc/stm32f407xx_gpio_driver.h
+++ drivers/Inc/stm32f407xx_gpio_driver.h
@@ -11,9 +11,9 @@
  * This is a Configuration structure for GPIO pin
  */
 typedef struct {
-	uint8_t GPIO_PinNumber;
-	uint8_t GPIO_PinMode;
-	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinNumber;					/*!< possible values from @GPIO_PIN_NUMBERS >*/
+	uint8_t GPIO_PinMode;					/*!< possible values from @GPIO_PIN_MODES >*/
+	uint8_t GPIO_PinSpeed;					/*!< possible values from @GPIO_PIN_SPEED >*/
 	uint8_t GPIO_PinPuPdControl;
 	uint8_t GPIO_PinOPType;
 	uint8_t GPIO_PinAltFunMode;
@@ -27,6 +27,61 @@ typedef struct {
 	GPIO_PinConfig_t GPIO_PinConfig;		/*!< This holds GPIO pin configuration settings >*/
 } GPIO_Handle_t;
 
+/**
+ * @GPIO_PIN_NUMBERS
+ * GPIO pin numbers
+ */
+#define GPIO_PIN_NO_0 						0
+#define GPIO_PIN_NO_1 						1
+#define GPIO_PIN_NO_2 						3
+#define GPIO_PIN_NO_3 						3
+#define GPIO_PIN_NO_4 						4
+#define GPIO_PIN_NO_5 						5
+#define GPIO_PIN_NO_6 						6
+#define GPIO_PIN_NO_7 						7
+#define GPIO_PIN_NO_8 						8
+#define GPIO_PIN_NO_9 						9
+#define GPIO_PIN_NO_10 						10
+#define GPIO_PIN_NO_11 						11
+#define GPIO_PIN_NO_12 						12
+#define GPIO_PIN_NO_13 						13
+#define GPIO_PIN_NO_14 						14
+#define GPIO_PIN_NO_15 						15
+
+
+/**
+ * @GPIO_PIN_MODES
+ * GPIO pin possible modes
+ */
+#define GPIO_MODE_INPUT 					0
+#define GPIO_MODE_OUTPUT 					1
+#define GPIO_MODE_ALTFN 					2
+#define GPIO_MODE_ANALOG 					3
+#define GPIO_MODE_IT_FT 					4
+#define GPIO_MODE_IT_RT 					5
+#define GPIO_MODE_IT_RFT 					6
+
+/**
+ * GPIO pin possible output types
+ */
+#define GPIO_OP_TYPE_PP						0
+#define GPIO_OP_TYPE_OD						1
+
+/**
+ * @GPIO_PIN_SPEED
+ * GPIO pin possible output speeds
+ */
+#define GPIO_SPEED_LOW						0
+#define GPIO_SPEED_MEDIUM					1
+#define GPIO_SPEED_FAST						2
+#define GPIO_SPEED_HIGH						3
+
+/**
+ * GPIO pin pull up AND pull down configuration macros
+ */
+#define GPIO_NO_PUPD						0
+#define GPIO_PIN_PU							1
+#define GPIO_PIN_PD							2
 
 /**********************************************************************************************
  * 								APIs supported by this driver
@@ -36,7 +91,7 @@ typedef struct {
 /**
  * Peripheral Clock setup
  */
-void GPIO_PeripheralClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
 
 /**
  * Init and De-init
